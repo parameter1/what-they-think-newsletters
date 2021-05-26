bfa89f72bcce0276efdfde0771bcd61444ca76fa
Brandon Krigbaum <brandonbk@gmail.com>
2021-05-26 14:56:58 -0500

    Add core.js

---
+++ tenants/pn/config/core.js
@@ -0,0 +1,10 @@
+const config = {
+  'finishing-and-mailing': {
+    name: 'Finishing and Mailing',
+  },
+  'labels-and-packaging': {
+    name: 'Labels and Packaging',
+  },
+};
+
+module.exports = config;
